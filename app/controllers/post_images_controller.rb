3b4ba1c963ed4dec6e244fcc855e11a33a7cb7b2
EC2 Default User <ec2-user@ip-172-31-32-147.ec2.internal>
2022-06-04 10:24:01 +0000

    update

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,15 +10,26 @@ class PostImagesController < ApplicationController
     @post_image.user_id = current_user.id
     # @post_image(投稿データ)のuser_idを、current_user.id(今ログインしているユーザーの ID)に
     # 指定することで投稿データに、今ログイン中のユーザーの ID を持たせることができる。
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+  
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  # get 'post_images/new'
-  # get 'post_images/index'
-  # get 'post_images/show'
-  resources :post_images,　only: [:new, :create, :index, :show]
   devise_for :users
+  root to:"homes#top"
+  get 'homes/about' => 'homes#about',as: :about
+  resources :post_images,　only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root to:"homes#top"
-    get 'homes/about' => 'homes#about',as: :about
 end
+
